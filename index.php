e99238d27a731b4c8576da802f476d48c8b7218c
Lukas Walkenbach <lukas.walkenbach@gmail.com>
2020-03-22 18:49:10 +0100

    Update index.php

--- index.php
+++ index.php
@@ -180,14 +180,19 @@ https://templatemo.com/tm-531-reflux
                   <a href="https://www.facebook.com/RemedyMatch-103244501323963/" target="_blank"><i class="fa fa-facebook"></i
                   ></a>
                 </li>
+                <!--
                 <li>
-                  <a href="https://twitter.com/RemedyMatch" target="_blank"><i class="fa fa-twitter"></i></a>
+                  <a href="https://twitter.com/RemedyMatch" target="_blank"><i class="fa fa-twitter"></i></a>  Twitter Main Account
                 </li>
+                -->
                 <li>
                   <a href="https://www.instagram.com/remedymatch/" target="_blank"><i class="fa fa-instagram"></i></a>
                 </li>
                 <li>
-                  <a href="https://twitter.com/RMatch2020 " target="_blank"><i class="fa fa-twitter"></i></a>
+                  <a href="https://twitter.com/RMatch2020" target="_blank"><i class="fa fa-twitter"></i></a>
+                </li>
+                <li>
+                  <a href="https://www.youtube.com/channel/UCA87nAMk_Q2obGjmBh0iN0A" target="_blank"><i class="fa fa-youtube"></i></a>
                 </li>
               </ul>
             </div>
