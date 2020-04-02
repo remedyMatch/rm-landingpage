44dc2df65bafe9f49758351365629166b412f66a
Roman Allenstein <mail@roman-allenstein.de>
2020-04-02 13:17:57 +0200

    #25 Added controller and service to handle keycloak registration

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -3,15 +3,20 @@
 namespace App\Controller;
 
 use App\Entity\Account;
+use App\Service\KeycloakRestApiService;
 use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
 use JoliCode\Slack\ClientFactory;
 use JoliCode\Slack\Exception\SlackErrorResponse;
 use PHPMailer\PHPMailer\Exception;
 use PHPMailer\PHPMailer\PHPMailer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response as ResponseAlias;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Address;
 use Symfony\Component\Mime\Email;
@@ -23,11 +28,37 @@ class RegisterController extends AbstractController
 {
     private $router;
     private $mailer;
+    private $session;
+    private $keycloakRestApi;
+    private $organisations = [
+        [
+            'value' => 1,
+            'label' => 'Pflegende Angehörige'
+        ],
+        [
+            'value' => 2,
+            'label' => 'Pflegedienst'
+        ],
+        [
+            'value' => 3,
+            'label' => 'Krankenhaus'
+        ],
+        [
+            'value' => 4,
+            'label' => 'Gewerbe und Industrie'
+        ],
+    ];
 
-    public function __construct(UrlGeneratorInterface $router, MailerInterface $mailer)
-    {
+    public function __construct(
+        UrlGeneratorInterface $router,
+        MailerInterface $mailer,
+        SessionInterface $session,
+        KeycloakRestApiService $keycloakRestApi
+    ) {
         $this->router = $router;
         $this->mailer = $mailer;
+        $this->session = $session;
+        $this->keycloakRestApi = $keycloakRestApi;
     }
 
     /**
@@ -37,85 +68,37 @@ class RegisterController extends AbstractController
      * @return ResponseAlias
      * @throws \Symfony\Component\Mailer\Exception\TransportExceptionInterface
      */
-    public function registrierung(Request $request, MailerInterface $mailer)
-    {
+    public function registrierung(
+        Request $request,
+        MailerInterface $mailer
+    ) {
         if ($request->get('firstname')) {
-            $this->handleRegistration($request);
-            return $this->render('register/bestaetigung-erforderlich.html.twig');
+            if ($this->handleRegistration($request)) {
+                return $this->render('register/bestaetigung.html.twig');
+            } else {
+                return $this->render('register/fehler.html.twig', [
+                    'message' => 'Ihre E-Mailadresse ist bereits registriert.'
+                ]);
+            }
         }
 
-        $organisations = [
-            [
-                'value' => 1,
-                'label' => 'Pflegende Angehörige'
-            ],
-            [
-                'value' => 2,
-                'label' => 'Pflegedienst'
-            ],
-            [
-                'value' => 3,
-                'label' => 'Krankenhaus'
-            ],
-            [
-                'value' => 4,
-                'label' => 'Gewerbe und Industrie'
-            ],
-        ];
-
         return $this->render('register/registrierung.html.twig', [
-            'organisations' => $organisations,
-            'success' => $request->get('success')
+            'organisations' => $this->organisations
         ]);
     }
 
     /**
      * @param Request $request
-     * @return void
-     * @throws \Symfony\Component\Mailer\Exception\TransportExceptionInterface
+     * @return bool
      */
-    private function handleRegistration(Request $request)
-    {
-        $entityManager = $this->getDoctrine()->getManager();
-
-        // Daten validieren
-        $uniqueId = uniqid();
-        $confirmLink = $this->router->generate('confirm', ['token' => $uniqueId], UrlGeneratorInterface::ABSOLUTE_URL);
-
-        // Daten abspeichern
-        $account = new Account();
-        $account->setFirstname($request->get('firstname'));
-        $account->setLastname($request->get('lastname'));
-        $account->setEmail($request->get('email'));
-        $account->setStreet($request->get('street'));
-        $account->setHousenumber($request->get('housenumber'));
-        $account->setZipcode($request->get('zipcode'));
-        $account->setCity($request->get('city'));
-        $account->setPhone($request->get('phone'));
-        $account->setPassword($request->get('password'));
-        $account->setType($request->get('type'));
-        $account->setCompany($request->get('company'));
-        $account->setToken($uniqueId);
-        try {
-            $entityManager->persist($account);
-            $entityManager->flush();
-        } catch (UniqueConstraintViolationException $exception) {
-
+    private function handleRegistration(
+        Request $request
+    ) {
+        // Add user to keycloak
+        if (!$this->createKeycloakAccount($request)) {
+            return false;
         }
-
-        // prepare email
-        $email = (new TemplatedEmail())
-            ->from(new Address('info@remedymatch.io', 'RemedyMatch.io'))
-            ->to(new Address($account->getEmail(), !empty($account->getCompany()) ? $account->getCompany() : $account->getFirstname() . ' ' . $account->getLastname()))
-            ->subject('Aktivieren Sie Ihren Zugang für RemedyMatch.io')
-            ->htmlTemplate('emails/account-confirm.html.twig')
-            ->context([
-                'firstname' => $account->getFirstname(),
-                'confirmLink' => $confirmLink
-            ]);
-
-        $this->mailer->send($email);
-
+        // send to slack
         // Slack message senden
         $token = $this->getParameter('app.slack_token');
         $client = ClientFactory::create($token);
@@ -124,40 +107,66 @@ class RegisterController extends AbstractController
         $result = $client->chatPostMessage([
             'username' => 'remedybot',
             'channel' => 'sandkasten',
-            'text' => 'Es hat sich ein neuer Benutzer (' . $uniqueId . ') registriert.',
+            'text' => 'Es hat sich ein neuer Benutzer registriert.',
         ]);
+        return true;
     }
 
     /**
-     * @Route("/confirm/{token}", name="confirm")
-     * @param string $token
-     * @return ResponseAlias
-     * @throws \Exception
+     * @param Request $request
+     * @return bool
      */
-    public function confirm(string $token)
+    private function createKeycloakAccount(Request $request)
     {
-        $entityManager = $this->getDoctrine()->getManager();
-        $account = $entityManager->getRepository(Account::class)->findOneBy(['token' => $token]);
-
-        if (!$account instanceof Account) {
-            return $this->render('register/fehler.html.twig');
+        $group = [
+            # 'access' => '',
+            # 'attributes' => '',
+            # 'clientRoles' => '',
+            # 'id' => '',
+            'name' => $request->get('company'),
+            # 'path' => 'private-persons',
+            # 'realmRoles' => [],
+            # 'subGroups' => [],
+        ];
+        $user = [
+            'email' => $request->get('email'),
+            'username' => $request->get('email'),
+            'firstName' => $request->get('firstname'),
+            'lastName' => $request->get('lastname'),
+            'enabled' => false,
+            'emailVerified' => false,
+            'credentials' => [
+                [
+                    'type' => 'password',
+                    'value' => $request->get('password'),
+                    'temporary' => false
+                ]
+            ],
+            'attributes' => [
+                'company' => $request->get('company'),
+                'company-type' => $request->get('type'),
+                'street' => $request->get('street'),
+                'housenumber' => $request->get('housenumber'),
+                'zipcode' => $request->get('zipcode'),
+                'city' => $request->get('city'),
+                'country' => 'germany'
+            ]
+        ];
+        /*
+        $user['groups'] = [
+            $group['name']
+        ];
+        */
+        try {
+            $this->keycloakRestApi->addGroup($group);
+        } catch (\Exception $exception) {
         }
 
-        $account->setVerifiedAt(new \DateTime());
-        $entityManager->persist($account);
-        $entityManager->flush();
-
-        // Slack message senden
-        $token = $this->getParameter('app.slack_token');
-        $client = ClientFactory::create($token);
-
-        // This method requires your token to have the scope "chat:write"
-        $result = $client->chatPostMessage([
-            'username' => 'remedybot',
-            'channel' => 'sandkasten',
-            'text' => 'Ein  Benutzer (' . $account->getToken() . ') hat seinen Account aktiviert.',
-        ]);
-
-        return $this->render('register/bestaetigung.html.twig');
+        try {
+            $this->keycloakRestApi->addUser($user);
+        } catch (ClientException $clientException) {
+            return false;
+        }
+        return true;
     }
 }
---
+++ src/Service/KeycloakRestApiService.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace App\Service;
+
+use Cocur\Slugify\Slugify;
+use Cocur\Slugify\SlugifyInterface;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+
+/**
+ * Class KeycloakRestApiService
+ * @package App\Service
+ *
+ *
+ * Jeder User erhält eine eigene Gruppe --> Privatperson Mailadresse als gruppenname
+ * -
+ *
+ */
+class KeycloakRestApiService
+{
+    /**
+     * @var ParameterBagInterface
+     */
+    protected $params;
+    /** @var Client */
+    private $client;
+    /** @var string */
+    private $accessToken;
+    /** @var string */
+    private $keycloakUrl;
+    /** @var string */
+    private $username;
+    /** @var string */
+    private $password;
+    /** @var string */
+    private $clientId;
+    /** @var string */
+    private $secret;
+
+    /**
+     * KeycloakRestApiService constructor.
+     * @param ParameterBagInterface $params
+     */
+    public function __construct(ParameterBagInterface $params)
+    {
+        $this->params = $params;
+        $this->keycloakUrl = $this->params->get('app.keycloak.url');
+        $this->username = $this->params->get('app.keycloak.user');
+        $this->password = $this->params->get('app.keycloak.password');
+        $this->clientId = $this->params->get('app.keycloak.client_id');
+        $this->secret = $this->params->get('app.keycloak.client_secret');
+        $this->accessToken = $this->fetchAccessToken();
+        $this->client = new \GuzzleHttp\Client([
+            'base_uri' => $this->keycloakUrl . '/auth/',
+            'defaults' => [
+                'headers' => [
+                    'Authorization' => 'Bearer ' . $this->accessToken,
+                ]
+            ]
+        ]);
+    }
+
+    /**
+     * @return string Token
+     */
+    public function fetchAccessToken()
+    {
+        $client = new Client();
+        $response = $client->request('POST', $this->keycloakUrl . '/auth/realms/master/protocol/openid-connect/token',
+            [
+                'auth' => ['remedymatch', 'development'],
+                'form_params' => [
+                    'username' => $this->username,
+                    'password' => $this->password,
+                    'grant_type' => 'password',
+                    'client_id' => $this->clientId,
+                    'secret' => $this->secret,
+                ]
+            ]);
+        $data = json_decode($response->getBody(), true);
+        $token = $data['access_token'];
+        return $token;
+    }
+
+    /**
+     * @param null $email
+     * @return array
+     */
+    public function getUsers($email = null): array
+    {
+        $response = $this->client->request('GET', 'admin/realms/master/users',
+            [
+                'auth' => ['remedymatch', 'development'],
+                'headers' => [
+                    'Authorization' => 'Bearer ' . $this->accessToken,
+                ],
+                'query' => [
+                    'email' => $email
+                ]
+            ]);
+        return json_decode($response->getBody()->getContents());
+    }
+
+    /**
+     * @param array $user
+     * @return string
+     * @throws ClientException
+     */
+    public function addUser(array $user)
+    {
+        $response = $this->client->request('POST', 'admin/realms/master/users',
+            [
+                'headers' => [
+                    'Authorization' => 'Bearer ' . $this->accessToken,
+                ],
+                'json' => $user
+            ]);
+        return $response->getBody()->getContents();
+    }
+
+    /**
+     * @param array $group
+     * @return string
+     */
+    public function addGroup(array $group)
+    {
+        $response = $this->client->request('POST', 'admin/realms/master/groups',
+            [
+                'headers' => [
+                    'Authorization' => 'Bearer ' . $this->accessToken,
+                ],
+                'json' => $group
+            ]);
+        return $response->getBody()->getContents();
+    }
+}
