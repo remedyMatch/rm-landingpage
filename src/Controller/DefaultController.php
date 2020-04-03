a114b2af2377222c8f22edd09861991778408a74
Lukas Walkenbach <lukas.walkenbach@gmail.com>
2020-04-03 23:46:21 +0200

    Update DefaultController.php

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -136,7 +136,7 @@ class DefaultController extends AbstractController
                 'title' => 'MySimpleShow',
                 'url' => 'https://www.mysimpleshow.com/de/',
                 'img' => '/assets/images/partners/simpleshow.png',
-                'description' => 'Danke, dass wir den mysimpleshow Online-Video-Creator kostenlos nutzen dürfen',
+                'description' => 'Simpleshow unterstützt uns durch die kostenlose Bereitstellung des mysimpleshow Online-Video-Creator.',
             ]
         ];
 
