713c98edb30e8ad7a5bed2288ce6e18c2ca6f599
Julian Haupt <julian@haupt.media>
2020-04-06 19:36:42 +0200

    add actions for reject, validate

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -7,6 +7,7 @@ use App\Repository\AccountRepository;
 use App\Service\KeycloakRestApiService;
 use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
 use GuzzleHttp\Exception\ClientException;
+use Http\Discovery\Exception\NotFoundException;
 use JoliCode\Slack\ClientFactory;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -31,10 +32,30 @@ class AdminController extends AbstractController
 
     /**
      * @Route("/admin", name="admin", methods={"GET"})
+     * @param Request $request
      * @return ResponseAlias
      */
-    public function admin()
+    public function admin(Request $request)
     {
+        switch($request->get('action')) {
+            case 'reject':
+                $account = $this->accountRepository->findOneByEmail($request->get('email'));
+                if(!$account)
+                    throw new NotFoundException();
+
+                echo "TODO: Reject Account".$account->getId();
+                break;
+
+            case 'validate':
+                $account = $this->accountRepository->findOneByEmail($request->get('email'));
+                if(!$account)
+                    throw new NotFoundException();
+
+                echo "TODO: Validate Account".$account->getId();
+
+                break;
+        }
+
         return $this->render('admin.html.twig', [
             'accounts' => $this->accountRepository->findUnreviewed()
         ]);
