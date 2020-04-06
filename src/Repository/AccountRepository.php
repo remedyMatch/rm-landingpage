878ae46e83227022b04a66ec1ac8dfb91984fcce
Julian Haupt <julian@haupt.media>
2020-04-06 19:36:42 +0200

    add actions for reject, validate

--- src/Repository/AccountRepository.php
+++ src/Repository/AccountRepository.php
@@ -9,6 +9,7 @@ use Doctrine\Common\Persistence\ManagerRegistry;
 /**
  * @method Account|null find($id, $lockMode = null, $lockVersion = null)
  * @method Account|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Account|null findOneByEmail(array $orderBy = null)
  * @method Account[]    findAll()
  * @method Account[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
