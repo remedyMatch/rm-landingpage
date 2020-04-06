5f529e0b2144b31ef33430f09699a4ce1e5921b3
Roman Allenstein <mail@roman-allenstein.de>
2020-04-06 21:06:43 +0200

    Added cache-buster for assets

--- install.sh
+++ install.sh
@@ -2,3 +2,5 @@
 composer install
 php bin/console doctrine:migrations:migrate --no-interaction
 php bin/console cache:clear
+VERSION="$(cat .git/ORIG_HEAD)"
+sed -i -e "/ASSET_VERSION=/s/=.*/=${VERSION:0:5}/" ".env.local"
