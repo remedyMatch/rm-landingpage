f2d473d1428a6921168ea8713dc856a77362ef42
Roman Allenstein <mail@roman-allenstein.de>
2020-04-07 15:53:25 +0200

    Added design-guide + changed color for button secondary

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -18,115 +18,119 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
     private $mailer;
-    private  $mentions = [
-            [
-                'title' => 'Bundesregierung',
-                'url' => 'https://www.bundesregierung.de/breg-de/themen/coronavirus/hackathon-ehrung-1738080',
-                'img' => '/assets/images/print/Die-Bundesregierung-Logo.png',
-                'description' => 'Die Arbeit beginnt jetzt',
-                'date' => '01.04.2020',
-                
-            ],
-            [
-                'title' => 'Berliner Zeitung',
-                'url' => 'https://www.berliner-zeitung.de/zukunft-technologie/1500-projekte-sind-beim-groesstem-hackathon-der-welt-entstanden-li.79615',
-                'img' => '/assets/images/print/berliner-zeitung.png',
-                'description' => '1500 Projekte sind beim größten Hackathon der Welt entstanden',
-                'date' => '27.03.2020',
-                
-            ],
-            [
-                'title' => 'Zukunft Krankenhaus Einkauf',
-                'url' => 'https://www.zukunft-krankenhaus-einkauf.de/2020/03/22/remedymatch-bringt-bedarf-an-schutzausr%C3%BCstung-und-spenden-zusammen/',
-                'img' => '/assets/images/print/zukunft-krankenhaus-einkauf-logo.jpg',
-                'description' => 'RemedyMatch bringt Bedarf an Schutzausrüstung und Spenden zusammen',
-                'date' => '22.03.2020',
-                
-            ],
-            [
-                'title' => 'Tagespiegel',
-                'url' => 'https://background.tagesspiegel.de/digitalisierung/hackathon-nun-startet-die-umsetzungsphase',
-                'img' => '/assets/images/print/TagesspiegelBackground.jpg',
-                'description' => 'Hackathon: Nun startet die Umsetzungsphase',
-                'date' => '31.03.2020',
-                
-            ],
-            [
-                'title' => 'DIE Zeit',
-                'url' => 'https://www.zeit.de/digital/internet/2020-03/hackathon-wirvsvirus-bundesregierung-gewinnerprojekte-ideen-umsetzung',
-                'img' => '/assets/images/print/Zeit-Onlinepng.png',
-                'description' => 'Die Corona-Hacks sind da',
-                'date' => '31.03.2020',
-                
-            ],
-            [
-                'title' => 'Ärzteblatt',
-                'url' => 'https://www.aerzteblatt.de/nachrichten/111528/Hackathon-foerdert-digitale-Loesungen-fuer-Krisenzeiten',
-                'img' => '/assets/images/print/aerzteblatt-logo.png',
-                'description' => 'Hackathon fördert digitale Lösungen für Krisenzeiten',
-                'date' => '01.04.2020',
-                
-            ],
-            [
-                'title' => 'Business Insider',
-                'url' => 'https://www.businessinsider.de/tech/hackathon-zur-corona-krise-das-sind-die-20-gewinner-des-wettbewerbs/',
-                'img' => '/assets/images/print/Business-insider-logo.png',
-                'description' => 'Hackathon zur Corona-Krise: Das sind die 20 Gewinner des Wettbewerbs',
-                'date' => '01.04.2020',
-                
-            ],
-            [
-                'title' => 'Computerwoche',
-                'url' => 'https://www.computerwoche.de/a/hackathon-gegen-die-coronavirus-krise,3548708',
-                'img' => '/assets/images/print/computerwoche-logo.png',
-                'description' => 'Hackathon gegen die Coronavirus-Krise',
-                'date' => '01.04.2020',
-                
-            ],
-            [
-                'title' => 'MedicalMountains',
-                'url' => 'https://medicalmountains.de/marktplatz/corona-drehscheibe/taskforces-fuer-die-medizintechnik/',
-                'img' => '/assets/images/print/MM-drehscheibe-logo.png',
-                'description' => 'Taskforces für die Medizintechnik',
-                'date' => '03.04.2020',
-                
-            ],
-             [
-                'title' => 'BVMed',
-                'url' => 'https://www.bvmed.de/de/technologien/hygieneprodukte/corona-informationen-des-bvmed-zu-praevention-und-umgang/_2-kurzarbeit-din-normen-kostenfrei-ausfuhren-efta-informationen',
-                'img' => '/assets/images/print/BVMed-logo.png',
-                'description' => 'Corona-Virus | Neue EU-Export-Anordnung | Förderungen und Kurzarbeit | Angebots-Abgabe | Betriebsablauf | Prävention',
-                'date' => '27.03.2020',
-                
-            ],
-            [
-                'title' => 'Galileo TV',
-                'url' => 'https://www.galileo.tv/corona/corona-hackathon-bringt-ergebnisse-diese-entwicklungen-helfen-in-der-krise/',
-                'img' => '/assets/images/print/Galileo-Logo.png',
-                'description' => 'Hackathon bringt geniale Ergebnisse im Kampf gegen Corona',
-                'date' => '03.04.2020',
+    private $mentions = [
+        [
+            'title' => 'Bundesregierung',
+            'url' => 'https://www.bundesregierung.de/breg-de/themen/coronavirus/hackathon-ehrung-1738080',
+            'img' => '/assets/images/print/Die-Bundesregierung-Logo.png',
+            'description' => 'Die Arbeit beginnt jetzt',
+            'date' => '01.04.2020',
 
-            ],
-            [
-                'title' => 'Chip',
-                'url' => 'https://www.chip.de/news/WirVsVirus-Hackathon-Das-sind-die-besten-Projekte_182568260.html',
-                'img' => '/assets/images/print/logo-chip.png',
-                'description' => 'Programmieren gegen Corona: #WirVsVirus Hackathon der Bundesregierung zeichnet die besten Projekte aus',
-                'date' => '31.03.2020',
+        ],
+        [
+            'title' => 'Berliner Zeitung',
+            'url' => 'https://www.berliner-zeitung.de/zukunft-technologie/1500-projekte-sind-beim-groesstem-hackathon-der-welt-entstanden-li.79615',
+            'img' => '/assets/images/print/berliner-zeitung.png',
+            'description' => '1500 Projekte sind beim größten Hackathon der Welt entstanden',
+            'date' => '27.03.2020',
 
-            ]
-           
-            
-        ];
+        ],
+        [
+            'title' => 'Zukunft Krankenhaus Einkauf',
+            'url' => 'https://www.zukunft-krankenhaus-einkauf.de/2020/03/22/remedymatch-bringt-bedarf-an-schutzausr%C3%BCstung-und-spenden-zusammen/',
+            'img' => '/assets/images/print/zukunft-krankenhaus-einkauf-logo.jpg',
+            'description' => 'RemedyMatch bringt Bedarf an Schutzausrüstung und Spenden zusammen',
+            'date' => '22.03.2020',
+
+        ],
+        [
+            'title' => 'Tagespiegel',
+            'url' => 'https://background.tagesspiegel.de/digitalisierung/hackathon-nun-startet-die-umsetzungsphase',
+            'img' => '/assets/images/print/TagesspiegelBackground.jpg',
+            'description' => 'Hackathon: Nun startet die Umsetzungsphase',
+            'date' => '31.03.2020',
+
+        ],
+        [
+            'title' => 'DIE Zeit',
+            'url' => 'https://www.zeit.de/digital/internet/2020-03/hackathon-wirvsvirus-bundesregierung-gewinnerprojekte-ideen-umsetzung',
+            'img' => '/assets/images/print/Zeit-Onlinepng.png',
+            'description' => 'Die Corona-Hacks sind da',
+            'date' => '31.03.2020',
+
+        ],
+        [
+            'title' => 'Ärzteblatt',
+            'url' => 'https://www.aerzteblatt.de/nachrichten/111528/Hackathon-foerdert-digitale-Loesungen-fuer-Krisenzeiten',
+            'img' => '/assets/images/print/aerzteblatt-logo.png',
+            'description' => 'Hackathon fördert digitale Lösungen für Krisenzeiten',
+            'date' => '01.04.2020',
+
+        ],
+        [
+            'title' => 'Business Insider',
+            'url' => 'https://www.businessinsider.de/tech/hackathon-zur-corona-krise-das-sind-die-20-gewinner-des-wettbewerbs/',
+            'img' => '/assets/images/print/Business-insider-logo.png',
+            'description' => 'Hackathon zur Corona-Krise: Das sind die 20 Gewinner des Wettbewerbs',
+            'date' => '01.04.2020',
+
+        ],
+        [
+            'title' => 'Computerwoche',
+            'url' => 'https://www.computerwoche.de/a/hackathon-gegen-die-coronavirus-krise,3548708',
+            'img' => '/assets/images/print/computerwoche-logo.png',
+            'description' => 'Hackathon gegen die Coronavirus-Krise',
+            'date' => '01.04.2020',
+
+        ],
+        [
+            'title' => 'MedicalMountains',
+            'url' => 'https://medicalmountains.de/marktplatz/corona-drehscheibe/taskforces-fuer-die-medizintechnik/',
+            'img' => '/assets/images/print/MM-drehscheibe-logo.png',
+            'description' => 'Taskforces für die Medizintechnik',
+            'date' => '03.04.2020',
+
+        ],
+        [
+            'title' => 'BVMed',
+            'url' => 'https://www.bvmed.de/de/technologien/hygieneprodukte/corona-informationen-des-bvmed-zu-praevention-und-umgang/_2-kurzarbeit-din-normen-kostenfrei-ausfuhren-efta-informationen',
+            'img' => '/assets/images/print/BVMed-logo.png',
+            'description' => 'Corona-Virus | Neue EU-Export-Anordnung | Förderungen und Kurzarbeit | Angebots-Abgabe | Betriebsablauf | Prävention',
+            'date' => '27.03.2020',
+
+        ],
+        [
+            'title' => 'Galileo TV',
+            'url' => 'https://www.galileo.tv/corona/corona-hackathon-bringt-ergebnisse-diese-entwicklungen-helfen-in-der-krise/',
+            'img' => '/assets/images/print/Galileo-Logo.png',
+            'description' => 'Hackathon bringt geniale Ergebnisse im Kampf gegen Corona',
+            'date' => '03.04.2020',
+
+        ],
+        [
+            'title' => 'Chip',
+            'url' => 'https://www.chip.de/news/WirVsVirus-Hackathon-Das-sind-die-besten-Projekte_182568260.html',
+            'img' => '/assets/images/print/logo-chip.png',
+            'description' => 'Programmieren gegen Corona: #WirVsVirus Hackathon der Bundesregierung zeichnet die besten Projekte aus',
+            'date' => '31.03.2020',
+
+        ]
+
+
+    ];
+
     public function __construct(MailerInterface $mailer)
     {
         $this->mailer = $mailer;
     }
-    function date_compare($element1, $element2) { 
-        $datetime1 = strtotime($element1['date']); 
-        $datetime2 = strtotime($element2['date']); 
-        return  $datetime2 -$datetime1; 
-    }  
+
+    function date_compare($element1, $element2)
+    {
+        $datetime1 = strtotime($element1['date']);
+        $datetime2 = strtotime($element2['date']);
+        return $datetime2 - $datetime1;
+    }
+
     /**
      * @Route("/", name="index")
      * @param Request $request
@@ -134,7 +138,7 @@ class DefaultController extends AbstractController
      */
     public function index(Request $request)
     {
-        usort($this->mentions, array($this,"date_compare")); 
+        usort($this->mentions, array($this, "date_compare"));
         $partners = [
             [
                 'title' => 'FlowSquad',
@@ -156,8 +160,7 @@ class DefaultController extends AbstractController
             ]
         ];
 
-        
-        
+
         return $this->render('index/index.html.twig', [
             'preregister' => $request->get('registered'),
             'partners' => $partners,
@@ -230,15 +233,17 @@ class DefaultController extends AbstractController
     {
         return $this->render('index/datenschutz.html.twig');
     }
-     /**
+
+    /**
      * @Route("/presse", name="presse")
      * @return Response
      */
     public function presse()
     {
-        usort($this->mentions, array($this,"date_compare")); 
-        return $this->render('press/presse.html.twig', ['mentions'=>$this->mentions]);
+        usort($this->mentions, array($this, "date_compare"));
+        return $this->render('press/presse.html.twig', ['mentions' => $this->mentions]);
     }
+
     /**
      * @Route("/mail", name="mail")
      * @return Response
@@ -247,6 +252,7 @@ class DefaultController extends AbstractController
     {
         return $this->render('emails/account-confirm.html.twig');
     }
+
     /**
      * @Route("contactHR", name="contactHR")
      * @param Request $request
@@ -274,6 +280,7 @@ class DefaultController extends AbstractController
 
         return $this->redirectToRoute('jobs', ['mailSent' => 1]);
     }
+
     /**
      * @Route("/offene-positionen", name="jobs")
      * @param Request $request
@@ -281,7 +288,7 @@ class DefaultController extends AbstractController
      */
     public function jobs(Request $request)
     {
-        return $this->render('jobs/jobs.html.twig',['emailSent' => $request->get('mailSent')]);
+        return $this->render('jobs/jobs.html.twig', ['emailSent' => $request->get('mailSent')]);
     }
 
     /**
@@ -291,6 +298,14 @@ class DefaultController extends AbstractController
      */
     public function contactForm(Request $request)
     {
-        return $this->render('blocks/contact.html.twig',['emailSent' => $request->get('mailSent')]);
+        return $this->render('blocks/contact.html.twig', ['emailSent' => $request->get('mailSent')]);
+    }
+
+    /**
+     * @Route("/design", name="design")
+     */
+    public function design()
+    {
+        return $this->render('index/design.twig');
     }
 }
