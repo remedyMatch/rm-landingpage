7f04fdaf52c35deaaff2bdb75176df515c6292da
Lukas Walkenbach <lukas.walkenbach@gmail.com>
2020-04-23 09:50:59 +0200

    Create Sorting.php

---
+++ src/Util/Sorting.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Util;
+
+final class Sorting
+{
+    private function __construct()
+    {
+    }
+
+    public static function dateCompareArrays(array $element1, array $element2): int
+    {
+        $datetime1 = strtotime($element1['date']);
+        $datetime2 = strtotime($element2['date']);
+
+        return $datetime2 - $datetime1;
+    }
+
+    public static function prioCompareArrays(array $element1, array $element2): int
+    {
+        $priority1 =(int)$element1['priority'];
+        $priority2 = (int)$element2['priority'];
+
+        return  $priority1-$priority2;
+    }
+}
