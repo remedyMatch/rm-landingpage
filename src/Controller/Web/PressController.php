5259a542f27ba58b8455473ecfcc344de45e4be9
Lukas Walkenbach <lukas.walkenbach@gmail.com>
2020-04-26 17:47:27 +0200

    Make Press Dynamic

--- src/Controller/Web/PressController.php
+++ src/Controller/Web/PressController.php
@@ -4,14 +4,21 @@ declare(strict_types=1);
 
 namespace App\Controller\Web;
 
-use App\StaticData\Mentions;
-use App\Util\Sorting;
+use App\Repository\MentionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 final class PressController extends AbstractController
 {
+
+    private $mentionRepository;
+
+    public function __construct( MentionRepository $mentionRepository)
+    {
+
+        $this->mentionRepository = $mentionRepository;
+    }
     /**
      * @Route({
      *     "/press",
@@ -20,9 +27,7 @@ final class PressController extends AbstractController
      */
     public function press(): Response
     {
-        $mentions = Mentions::DATA;
-        usort($mentions, [Sorting::class, 'dateCompareArrays']);
-
+        $this->mentionRepository->findAll();
         return $this->render('web/press/press.html.twig', ['mentions' => $mentions]);
     }
 }
